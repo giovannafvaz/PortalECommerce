e8a3358f89978dcbbd859788091b2538354588e8
Giovanna Vaz <giovannafabiovaz@gmail.com>
2023-06-13 23:17:16 -0300

    Finalizado site

--- JS/detalhes.js
+++ JS/detalhes.js
@@ -6,8 +6,21 @@ function onLoadDetalhes(){
     let APIResponse = fazGet("http://diwserver.vps.webdock.cloud:8765/products/" + idProduto);
     //Atribui o resultado da chamada da API à variável responseText
     let responseText = JSON.parse(APIResponse.responseText);
-    console.log(responseText);
+
+    let imgProduto = document.getElementById("imgProduto");
+    imgProduto.src = responseText.image;
+    
+    let detalhesProduto = document.getElementById("detalhesProduto");
+    detalhesProduto.innerHTML = 
+    "<h3><b>" + responseText.title + " </b><br></h3>" +
+    "<b>Price:</b> U$" + responseText.price + "<br>" +
+    "<b>Color: </b> " + responseText.baseColour + "<br>" +           
+    "<b>Gender: </b> " + responseText.gender + " <br>" +
+    "<b>Usage: </b> " + responseText.usage + " <br>" +
+    "<b>Year: </b> " + responseText.year + " <br>" +
+    "<b>Description: </b>" + responseText.description;
 }
+
 
 
 onLoadDetalhes();
